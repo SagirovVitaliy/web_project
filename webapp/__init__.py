84661d82d9b62bd2bc1f8835227613ec34126e41
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-09-27 17:21:27 +0300

    Setup of Flask server seems stable

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
     @app.route('/')
     def index():
-        world = 'ZA WARUDO'
+        world = app.config['DEMO_WORLD']
         return render_template('hello_world.html', world=world);
     return app
