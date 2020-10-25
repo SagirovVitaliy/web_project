b842ea397ed23aeb470c5a95de86ccfe9442576c
agate-agate <70856230+agate-agate@users.noreply.github.com>
2020-10-25 23:42:59 +0300

    Менеджер Фикстур (#14)
    Продумал и организовал систему чтобы можно было удобно загружать фикстуры для тестов.
    
    * Created neat toolset for easily swapping between fixtures
    
    * Framework for flexible fixtures
    
    * Fix README bugs
    
    * Fix README bugs
    
    * Responce to review comments - improved code style
    
    * Fixed README bugs
    
    * Fixed comment grammar

--- load_fixture.py
+++ load_fixture.py
@@ -1,50 +1,129 @@
 from webapp import create_app
 from webapp.model import db, Email, Phone, Role, User, Tag, Status, Task
 from dateparser import parse
+from sys import argv
 
 import json
 
-with open("fixtures.json", "r", encoding='utf-8') as read_file:
-    data = json.load(read_file)
+
+'''
+    Ожидается что этот файл будет использоваться в коммандной строке в стиле:
+    python3 load_fixture.py
+    или
+    python3 load_fixture.py abcd
+    где вместо "abcd" надо присылать название файла фикстуры.
+
+    Файл с фиктурой ищется в папке fixtures/__имя_файла__.json
+
+    Для примера с abcd это будет fixtures/abcd.json
+
+    python3 load_fixture.py
+    - особый случай: будет подключаться фикстура по адресу fixtures/default.json
+'''
 
 
-def get_data(data):
-    for value in data['email']:
-        email = Email(email=value['email'])
-        db.session.add(email)
-        db.session.commit()
-    
-    for value in data['phone']:
-        phone = Phone(phone=value['phone'])
-        db.session.add(phone)
-        db.session.commit()
+def get_fixture_file_name():
+    argument_list = argv
+    fixture_file_name = 'default'
 
-    for value in data['user_role']:
-        user_role = Role(role=value['role'])
-        db.session.add(user_role)
-        db.session.commit()
-    
-    for value in data['user']:
-        user = User(username=value['username'], password=value['password'], public_bio=value['public_bio'])
-        db.session.add(user)
-        db.session.commit()
+    if len(argument_list) > 1:
+        fixture_file_name = f'{argv[1]}'
 
-    for value in data['tag']:
-        tag = Tag(tag=value['tag'])
-        db.session.add(tag)
-        db.session.commit()
+    return fixture_file_name
 
-    for value in data['task_status']:
-        status = Status(status=value['status'])
-        db.session.add(status)
-        db.session.commit()
 
-    for value in data['task']:
-        task = Task(task_name=value['task_name'], description=value['description'], price=value['price'], deadline=parse(value['deadline']))
-        db.session.add(task)
-        db.session.commit()
+def get_data_from_file(file_name):
+    fixture_folder = 'fixtures/'
+    fixture_file_name = f'{file_name}'
+    fixture_file_extension = '.json'
+    fixture_path = fixture_folder + fixture_file_name + fixture_file_extension
+
+    with open(fixture_path, "r", encoding='utf-8') as read_file:
+        data = json.load(read_file)
+
+    return data
+
+
+def push_item_to_db(model_class, dictionary_item, conversion_rules={}):
+    item = model_class()
+    for prop_name in dictionary_item:
+
+        prop_value = dictionary_item[prop_name]
+
+        # В редких случаях могут прислать правила в формате который нельзя
+        # посылать напрямую в db - например когда присылают дату в формате
+        # '2020-10-10', а db ожидает объект даты. В таких случаях - присылайте
+        # пометку о том что нужно сделать определённое преобразование.
+        conversion_rule = conversion_rules.get(prop_name, None)
+        if conversion_rule == 'date_iso8601':
+            prop_value = parse(prop_value)
+
+        setattr(item, prop_name, prop_value)
+    print('New item:')
+    print(item)
+    db.session.add(item)
+    db.session.commit()
+
+
+def push_table_to_db(
+        data,
+        table_name_in_data,
+        model_class,
+        conversion_rules={}
+    ):
+    table_elements = data.get(table_name_in_data, []);
+    for element in table_elements:
+        push_item_to_db(
+            model_class=model_class,
+            dictionary_item=element,
+            conversion_rules=conversion_rules
+        )
+
+
+def push_data_to_db(data):
+
+    push_table_to_db(
+        data=data,
+        table_name_in_data='email',
+        model_class=Email
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='phone',
+        model_class=Phone
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='user_role',
+        model_class=Role
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='user',
+        model_class=User
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='tag',
+        model_class=Tag
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='task_status',
+        model_class=Status
+    )
+    push_table_to_db(
+        data=data,
+        table_name_in_data='task',
+        model_class=Task,
+        conversion_rules={
+            'deadline': 'date_iso8601',
+        }
+    )
 
 
 app = create_app()
 with app.app_context():
-    get_data(data)
+    fixture_file_name = get_fixture_file_name()
+    data = get_data_from_file(file_name=fixture_file_name)
+    push_data_to_db(data)
