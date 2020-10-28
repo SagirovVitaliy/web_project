6d09e041e1d212f81f8ad5d55facdaa9fac8ac14
Michael Alekseevich <sapphire.agate.agate@gmail.com>
2020-10-29 00:03:41 +0300

    System to import m2m relationships from fixtures

--- load_fixture.py
+++ load_fixture.py
@@ -1,11 +1,3 @@
-from webapp import create_app
-from webapp.model import db, Email, Phone, UserRole, User, Tag, TaskStatus, Task
-from dateparser import parse
-from sys import argv
-
-import json
-
-
 '''
     Ожидается что этот файл будет использоваться в коммандной строке в стиле:
     python3 load_fixture.py
@@ -20,6 +12,15 @@ import json
     python3 load_fixture.py
     - особый случай: будет подключаться фикстура по адресу fixtures/default.json
 '''
+from webapp import create_app
+from webapp.model import (
+    db, Email, Phone, UserRole, User, Tag, TaskStatus, Task,
+    freelancers_who_responded
+    )
+from dateparser import parse
+from sys import argv
+
+import json
 
 
 def get_fixture_file_name():
@@ -44,11 +45,11 @@ def get_data_from_file(file_name):
     return data
 
 
-def push_item_to_db(model_class, dictionary_item, conversion_rules={}):
+def push_table_row_to_db(model_class, table_row, conversion_rules={}):
     item = model_class()
-    for prop_name in dictionary_item:
+    for prop_name in table_row:
 
-        prop_value = dictionary_item[prop_name]
+        prop_value = table_row[prop_name]
 
         # В редких случаях могут прислать правила в формате который нельзя
         # посылать напрямую в db - например когда присылают дату в формате
@@ -71,17 +72,102 @@ def push_table_to_db(
         model_class,
         conversion_rules={}
     ):
-    table_elements = data.get(table_name_in_data, []);
-    for element in table_elements:
-        push_item_to_db(
+    """Добавить в Базу Данных: Записи."""
+    table_rows = data.get(table_name_in_data, []);
+    for table_row in table_rows:
+        push_table_row_to_db(
             model_class=model_class,
-            dictionary_item=element,
+            table_row=table_row,
             conversion_rules=conversion_rules
         )
 
 
-def push_data_to_db(data):
+def push_m2m_relationship_to_db(
+        model_class_a,
+        model_class_a_prop_name_in_data,
+        model_class_a_prop_name_in_class,
+        model_class_a_relationship_in_class,
+        
+        model_class_b,
+        model_class_b_prop_name_in_data,
+        model_class_b_prop_name_in_class,
+        
+        table_row
+    ):
+    """Добавить в Базу Данных: одну Связь (между двумя предметами А и Б).
 
+    Keyword arguments:
+
+    model_class_a -- класс предмета А (без дефолта)
+    model_class_a_prop_name_in_data -- имя свойства с id Предмета А в table_row.
+    model_class_a_prop_name_in_class -- имя свойства с id Предмета А в классе А.
+    model_class_a_relationship_in_class --
+
+    model_class_b -- класс предмета Б (без дефолта)
+    model_class_b_prop_name_in_data -- имя свойства с id Предмета Б в table_row.
+    model_class_b_prop_name_in_class -- имя свойства с id Предмета Б в классе Б.
+
+    table_row -- ряд таблицы, описывающий связь двух предметов; в ряду -
+                 2 элемента - id Предмета A и id Предмета Б.
+    """
+    item_a = model_class_a.query.filter(
+        getattr(model_class_a, model_class_a_prop_name_in_class) ==
+        table_row[model_class_a_prop_name_in_data]
+    ).first()
+    
+    item_b = model_class_b.query.filter(
+        getattr(model_class_b, model_class_b_prop_name_in_class) ==
+        table_row[model_class_b_prop_name_in_data]
+    ).first()
+
+    getattr(item_a, model_class_a_relationship_in_class).append(item_b)
+    db.session.commit()
+    pass
+
+
+def push_m2m_relationships_to_db(
+        data,
+        table_name_in_data,
+        
+        model_class_a,
+        model_class_a_prop_name_in_data,
+        model_class_a_prop_name_in_class,
+        model_class_a_relationship_in_class,
+        
+        model_class_b,
+        model_class_b_prop_name_in_data,
+        model_class_b_prop_name_in_class
+    ):
+    """Добавить в Базу Данных: Отношения многие-ко-многим."""
+    table_rows = data.get(table_name_in_data, []);
+    for table_row in table_rows:
+        push_m2m_relationship_to_db(
+            model_class_a=model_class_a,
+            model_class_a_prop_name_in_data=model_class_a_prop_name_in_data,
+            model_class_a_prop_name_in_class=model_class_a_prop_name_in_class,
+            model_class_a_relationship_in_class=model_class_a_relationship_in_class,
+            
+            model_class_b=model_class_b,
+            model_class_b_prop_name_in_data=model_class_b_prop_name_in_data,
+            model_class_b_prop_name_in_class=model_class_b_prop_name_in_class,
+            
+            table_row=table_row
+        )
+
+
+def push_data_to_db(data):
+    """Добавить в Базу Данных: Записи и Отношения многие-ко-многим.
+
+    Это функция-клей.
+
+    Эта функция - не общего назначения. В том виде как она записана сейчас, она
+    не подойдёт другим сайтам. Она заточена под нашу структуру БД; и даже больше
+    того - под конкретную миграцию. Она ожидает наши названия таблиц и модель.
+    Подробней, про текущую структуру БД, смотрите файл webapp/model.py.
+
+    Но хотя эта функция заточена под конкретный сайт и конкретную миграцию, её
+    можно использовать как пример использования функций из этого модуля.
+    """
     push_table_to_db(
         data=data,
         table_name_in_data='email',
@@ -119,6 +205,19 @@ def push_data_to_db(data):
         conversion_rules={
             'deadline': 'date_iso8601',
         }
+    )
+
+    # Связи Many-To-Many надо подгружать после всех push_table_to_db.
+    push_m2m_relationships_to_db(
+        data=data,
+        table_name_in_data='m2m_freelancers_who_responded',
+        model_class_a=User,
+        model_class_a_prop_name_in_data='user_id',
+        model_class_a_prop_name_in_class='id',
+        model_class_a_relationship_in_class='responded_to_tasks',
+        model_class_b=Task,
+        model_class_b_prop_name_in_data='task_id',
+        model_class_b_prop_name_in_class='id'
     )
 
 
