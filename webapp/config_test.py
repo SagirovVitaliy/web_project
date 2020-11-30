04b37f95ef830a55d07cd82780585f58d28730ec
vitaliy <bitalko15@gmail.com>
2020-12-01 00:05:12 +0300

    вернул конфиг

---
+++ webapp/config_test.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'ergkmk.mrlmremoi.oicJNVSwqnwmf'
+TESTING = True
+WTF_CSRF_ENABLED = False
